33c014cbfb4ce97fe88e03ce297614cbcde3df2d
Sombrero <adriana@grecea.net>
2023-03-06 22:17:10 +0100

    added basic forward movement for enemy

---
+++ Assets/Scripts/FlamingoController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class FlamingoController : MonoBehaviour
+{
+    [SerializeField]
+    private float moveSpeed;
+    private Vector2 moveDirection;
+    private Rigidbody2D flamingo;
+    // Start is called before the first frame update
+    void Start() {
+        flamingo = GetComponent<Rigidbody2D>();
+        flamingo.freezeRotation = true;
+        moveDirection = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += moveSpeed * Time.deltaTime * new Vector3(moveDirection.y,0f , 0f);
+    }
+}
