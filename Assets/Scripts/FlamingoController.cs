d519effc2e116fb221c433cba8c1d04dce1ffd09
Sombrero <adriana@grecea.net>
2023-03-12 19:40:09 +0100

    refactor tile collider for enemy movement

--- Assets/Scripts/FlamingoController.cs
+++ Assets/Scripts/FlamingoController.cs
@@ -12,18 +12,25 @@ public class FlamingoController : MonoBehaviour
     void Start() {
         flamingo = GetComponent<Rigidbody2D>();
         flamingo.freezeRotation = true;
-        moveDirection = transform.position;
+        moveDirection = Vector2.left;
+        moveDirection.Normalize();
         flamingoSprite = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        transform.position += moveSpeed * Time.deltaTime * new Vector3(moveDirection.x,0f,0f);
+    private void Update() {
+        
+    }
+
+    void FixedUpdate() {
+        flamingo.MovePosition(flamingo.position + moveSpeed * Time.fixedDeltaTime * moveDirection);
+        // flamingo.velocity += new Vector2(moveSpeed * Time.deltaTime * moveDirection.x, flamingo.velocity.y);
+        Debug.Log(Time.fixedDeltaTime +" "+ moveSpeed +" "+ moveDirection  + " "+ flamingo.position);
+        // transform.position += moveSpeed * Time.fixedDeltaTime * new Vector3(moveDirection.x,0f,0f);
     }
 
     private void OnCollisionEnter2D(Collision2D col) {
-        if (col.gameObject.tag.Equals("Tile")) {
+        if (!col.gameObject.tag.Equals("Ground")) {
             flamingoSprite.flipX = !flamingoSprite.flipX;
             moveDirection.x *= -1;
         }
