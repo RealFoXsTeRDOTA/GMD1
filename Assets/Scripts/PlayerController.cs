844a058b3f468e8b2573b867eb46d4ac199643df
Georgiana <ion.georgiana@hotmail.com>
2023-03-21 17:12:46 +0100

    Slippery terrain

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,9 +10,16 @@ public class PlayerController : MonoBehaviour
 
   [SerializeField]
   private float jumpForce;
+  
+  [SerializeField]
+  private PhysicsMaterial2D catMaterialPhysics;
 
   private Vector2 moveDirection;
   private Rigidbody2D body;
+  private bool isOnIce = false;
+
+
+
 
   private void Start()
   {
@@ -23,11 +30,26 @@ public class PlayerController : MonoBehaviour
 
     body = GetComponent<Rigidbody2D>();
     body.freezeRotation = true;
+    
   }
 
   private void Update()
   {
-    transform.position += moveSpeed * Time.deltaTime * new Vector3(moveDirection.x, 0f, 0f);
+    if (isOnIce)
+    {
+      float horizontalInput = Input.GetAxis("Horizontal");
+      moveDirection = new Vector2(horizontalInput, 0f).normalized;
+    }
+    else
+    {
+      transform.position += moveSpeed * Time.deltaTime * new Vector3(moveDirection.x, 0f, 0f);
+    }
+
+  }
+
+  private void FixedUpdate()
+  {
+    body.AddForce(moveDirection * moveSpeed, ForceMode2D.Force);
   }
 
   private void HandleMove(Vector2 direction)
@@ -48,5 +70,23 @@ public class PlayerController : MonoBehaviour
   private void HandleAttack()
   {
     Debug.Log("Avada Kedavra!");
+  }
+
+  private void OnTriggerEnter2D(Collider2D col)
+  {
+    if (col.gameObject.tag.Equals("Ice") && catMaterialPhysics != null)
+    {
+      isOnIce = true;
+      body.drag = 0f;
+      body.sharedMaterial = catMaterialPhysics;
+    }
+  }
+
+  private void OnTriggerExit2D(Collider2D collision)
+  {
+    Debug.Log("OnTriggerExit2D");
+      isOnIce = false;
+      body.drag = 4f;
+      body.sharedMaterial = null;
   }
 }
