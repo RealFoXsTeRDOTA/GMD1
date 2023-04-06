22639f2fb51fee5065c4a87a74a24fac7e27b351
RealFoXsTeRDOTA <foxsterdota@gmail.com>
2023-04-06 17:16:20 +0200

    Added slime animations on damage and death

--- Assets/Scripts/Enemy/EnemyHealth.cs
+++ Assets/Scripts/Enemy/EnemyHealth.cs
@@ -3,8 +3,14 @@ using UnityEngine;
 public class EnemyHealth : MonoBehaviour
 {
   [SerializeField]
-  private float maxHealth = 2;
+  private float maxHealth = 5;
   private float currentHealth;
+
+  [SerializeField]
+  private ParticleSystem particlesOnDamage;
+
+  [SerializeField]
+  private Transform particlesOnDeath;
 
   private void Start()
   {
@@ -19,10 +25,16 @@ public class EnemyHealth : MonoBehaviour
     {
       Kill();
     }
+    else
+    {
+      particlesOnDamage.Play();
+    }
   }
 
   private void Kill()
   {
+    var spawnPosition = new Vector3(transform.position.x, transform.position.y - .4f, transform.position.z);
+    Instantiate(particlesOnDeath, spawnPosition, Quaternion.identity);
     Destroy(gameObject);
   }
 }
