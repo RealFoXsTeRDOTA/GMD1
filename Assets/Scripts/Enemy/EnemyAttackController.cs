8a4233d61d7a0ced401b2fa7a08c7958e0597118
Sombrero <adriana@grecea.net>
2023-04-06 15:20:01 +0200

    refactor enemy attack behaviour

--- Assets/Scripts/Enemy/EnemyAttackController.cs
+++ Assets/Scripts/Enemy/EnemyAttackController.cs
@@ -1,19 +1,37 @@
 using UnityEngine;
 
 public class EnemyAttackController : MonoBehaviour {
-    [SerializeField]
-    private float maxHealth;
-    private float health;
 
-    private bool isHit;
+    [SerializeField] private GameObject projectile;
+    private Rigidbody2D enemy;
+    private float timer;
+    
     // Start is called before the first frame update
     void Start() {
-        health = maxHealth;
+        enemy = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+       
     }
+
+    private void FireProjectile() {
+        Instantiate(projectile, enemy.position, Quaternion.identity);
+    }
+
+    private void OnTriggerStay2D(Collider2D other) {
+        if (!other.gameObject.tag.Equals("Player"))
+            return;
+        timer += Time.deltaTime;
+        if (timer > 1) {
+            timer = 0;
+            FireProjectile();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other) {
+        // timer = 0;
+    }
+
 }
