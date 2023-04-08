17c8d6de4e1b460882cf3f873c8b5f6e05cc2ef9
Sombrero <adriana@grecea.net>
2023-04-08 21:49:39 +0200

    refactor enemy attack script

--- Assets/Scripts/Enemy/DirectionController.cs
+++ Assets/Scripts/Enemy/DirectionController.cs
@@ -1,24 +1,24 @@
+
 using UnityEngine;
 
-public class DirectionController : MonoBehaviour
-{
-    private SpriteRenderer sprite;
-
-    // Start is called before the first frame update
-    void Start() {
-        sprite = GetComponent<SpriteRenderer>();
-    }
-
+public class DirectionController : MonoBehaviour {
+    /// <summary>
+    /// Ignore collision with the player 
+    /// </summary>
+    /// <param name="other"></param>
     private void OnCollisionEnter2D(Collision2D other) {
         if (other.collider.CompareTag("Player")) {
-            Physics2D.IgnoreCollision(other.gameObject.GetComponent<Collider2D>(), GetComponent<Collider2D>()); 
+            Physics2D.IgnoreCollision(other.gameObject.GetComponent<Collider2D>(), GetComponent<Collider2D>());
         }
     }
 
+    /// <summary>
+    /// update the scale of the ghost to always face the player while it is within trigger area
+    /// </summary>
     private void OnTriggerStay2D(Collider2D other) {
-        if (other.CompareTag("Player")) {
-            sprite.flipX = other.gameObject.transform.position.x < transform.position.x;
-        }
+        if (!other.CompareTag("Player"))
+            return;
+        transform.localScale = other.transform.position.x < transform.position.x ? new Vector3(-1, 1) : new Vector3(1, 1);
+
     }
-    
 }
