5d763c086f25dea9063b1cdec08c2c7e0ec9a42b
Sombrero <adriana@grecea.net>
2023-04-18 08:52:04 +0200

    update obstacle controller

--- Assets/Scripts/Enemy/ObstacleController.cs
+++ Assets/Scripts/Enemy/ObstacleController.cs
@@ -1,10 +1,26 @@
+using System;
 using UnityEngine;
 
-public class ObstacleController : MonoBehaviour
-{
+public class ObstacleController : MonoBehaviour {
+
+    [SerializeField] private int damageOnHit;
+    [SerializeField] private float timeBetweenHits;
+    private float hitCooldown;
+
+    private void Start() {
+        hitCooldown = 0;
+    }
+
+    private void Update() {
+        hitCooldown += Time.deltaTime;
+    }
+
     private void OnCollisionEnter2D(Collision2D other) {
         if (other.gameObject.TryGetComponent(out Health playerHealth)) {
-            playerHealth.TakeDamage(1);
+            if (hitCooldown >= timeBetweenHits) {
+                playerHealth.TakeDamage(damageOnHit);
+                hitCooldown = 0;
+            }
         } 
     }
 }
