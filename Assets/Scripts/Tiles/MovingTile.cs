bd024f1f2f7f53290e8833e14def3b5855be979a
Georgiana <ion.georgiana@hotmail.com>
2023-05-06 15:33:19 +0200

    Changed the level order

--- Assets/Scripts/Tiles/MovingTile.cs
+++ Assets/Scripts/Tiles/MovingTile.cs
@@ -6,24 +6,24 @@ using UnityEngine;
 public class MovingTile : MonoBehaviour, IEnterExitTile
 {
     [SerializeField]
-    private float speed = 2f;
+    private float slideDistance = 5f;
     [SerializeField]
-    private float moveDistance = 5f;
+    private float speed = 2f;
     [SerializeField]
     private bool rightFirst = true;
 
-    private Vector3 startPosition;
+    private Vector3 _initialPosition;
 
     private void Start()
     {
-        startPosition = transform.position;
+        _initialPosition = transform.position;
     }
 
     private void Update()
     {
         float direction = rightFirst ? 1f : -1f;
-        float newPosition = Mathf.PingPong(Time.time * speed, moveDistance);
-        transform.position = startPosition + Vector3.right * (direction * newPosition);
+        float newPosition = Mathf.PingPong(Time.time * speed, slideDistance);
+        transform.position = _initialPosition + Vector3.right * (direction * newPosition);
     }
     
     public void OnEnter(PlayerController playerController)
